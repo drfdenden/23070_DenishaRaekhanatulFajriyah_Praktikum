071fded8030a845d59157f6d0889e8df2df356c4
Denisha RF <67154111+drfdenden@users.noreply.github.com>
2024-11-10 18:52:06 +0700

    first commit

---
+++ src/Praktikum11/Koneksi.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Praktikum11;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author User
+ */
+public class Koneksi {
+    private static java.sql.Connection koneksi;
+    
+    public static java.sql.Connection getKoneksi(){
+        if (koneksi == null){
+            try{
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                String url = "jdbc:mysql://localhost:3306/database3d";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Connection Successfully");
+            }catch (Exception e){
+                System.out.println("Error");
+            }
+        }
+        return koneksi;
+    }
+    
+}
